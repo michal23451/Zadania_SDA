c3d05be247fad135ebecf406304d46711f2649b8
Michał Konowalski <konowalski.michal@gmail.com>
2023-02-08 00:29:03 +0100

    zadanie6.1

---
+++ Java zaawansowana - zadania/java_zaawansowana_zadania/src/oop/zad6/Movable.java
@@ -0,0 +1,11 @@
+package oop.zad6;
+
+public interface Movable {
+    void moveUp();
+
+    void moveDown();
+
+    void moveLeft();
+
+    void moveRight();
+}
---
+++ Java zaawansowana - zadania/java_zaawansowana_zadania/src/oop/zad6/MovablePoint.java
@@ -0,0 +1,25 @@
+package oop.zad6;
+
+public class MovablePoint implements Movable{
+
+
+    @Override
+    public void moveUp() {
+
+    }
+
+    @Override
+    public void moveDown() {
+
+    }
+
+    @Override
+    public void moveLeft() {
+
+    }
+
+    @Override
+    public void moveRight() {
+
+    }
+}
